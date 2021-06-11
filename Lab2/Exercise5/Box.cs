27203d8f490d0c6620dad5099527d01ebbb0a3a2
Rufeng Ma <buguaixiaoya@gmail.com>
2021-06-11 09:50:49 -0400

    Add files via upload

--- Lab2/Exercise5/Box.cs
+++ Lab2/Exercise5/Box.cs
@@ -2,12 +2,46 @@
 
 namespace Exercise5
 {
-
-    class MainClass
+    class Box
     {
+        public double length=0, breadth=0, height=0;
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public void setBreadth(double bre)
+        {
+            breadth = bre;
+        }
+
+        public void setHeight(double hei)
+        {
+            height = hei;
+        }
+
+
+    }
+    class Mainentry {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Box box = new Box();
+
+            Console.WriteLine("Initial volume is "+box.getVolume());
+            Console.WriteLine("Enter the length");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the breadth");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Height");
+            double h = Convert.ToDouble(Console.ReadLine());
+            box.setLength(l);
+            box.setBreadth(b);
+            box.setHeight(h);
+            Console.WriteLine("Now the volume is " + box.getVolume());
         }
     }
 }
