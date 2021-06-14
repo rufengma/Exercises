3d94450f410c69db269c798582a48ca8dbf6f0e4
Rufeng Ma <buguaixiaoya@gmail.com>
2021-06-14 09:32:12 -0400

    Add files via upload

---
+++ Lab4/Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewLanguageFeatures
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
