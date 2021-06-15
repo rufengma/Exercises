26746bed3427fceb629b27e063e4fcf074cbf42b
Rufeng Ma <buguaixiaoya@gmail.com>
2021-06-15 13:29:03 -0400

    Add files via upload

---
+++ Lab5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace Exercise6
+{
+    class A {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand("sq_Myproc", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
