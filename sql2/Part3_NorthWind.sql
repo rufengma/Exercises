452450a0bd5dfdfc805c38ce888d4e141b12ab79
Rufeng Ma <buguaixiaoya@gmail.com>
2021-06-18 09:55:41 -0400

    Add files via upload

--- sql2/Part3_NorthWind.sql
+++ sql2/Part3_NorthWind.sql
@@ -21,9 +21,81 @@ having o2.ShipPostalCode!='null'
 order by sum(o.Quantity) desc
 
 *q16*
-*/
+
+select top 5 o2.ShipPostalCode, sum(o.Quantity) as "total"
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+where o2.OrderDate between '1995-06-18' and '2021-06-18'
+group by  o2.ShipPostalCode
+having o2.ShipPostalCode!='null'
+order by sum(o.Quantity) desc
+
+*q17*
+select top 5 o2.ShipPostalCode,o2.ShipCity,count(o2.CustomerID) as "total Customer",sum(o.Quantity) as "total"
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+group by  o2.ShipPostalCode,o2.ShipCity
+having o2.ShipPostalCode!='null'
+order by sum(o.Quantity) desc
+
+*q18*
+
+select o2.ShipPostalCode,o2.ShipCity,count(o2.CustomerID) as "total Customer",sum(o.Quantity) as "total"
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+group by  o2.ShipPostalCode,o2.ShipCity
+having o2.ShipPostalCode!='null' and count(o2.CustomerID)>10
+order by sum(o.Quantity) desc
 
 
+*q19*
+select o2.ShipName, o2.OrderDate
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+where o2.OrderDate between '1998-01-01' and '2021-06-18'
 
+*q20*
+select o2.ShipName, o2.OrderDate
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+order by o2.OrderDate DESC
 
+*q21*
+select o2.CustomerID, sum(o.Quantity) as "Bought Products"
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+group by o2.CustomerID
 
+*q22*
+select o2.CustomerID, sum(o.Quantity) as "Bought Products"
+from Products p
+left join [Order Details] o
+on o.ProductID=p.ProductID
+left join Orders o2
+on o2.OrderID=o.OrderID
+group by o2.CustomerID
+having sum(o.Quantity)>100
+
+*q23*/
+select o.ShipAddress,s1.Address
+from Orders o 
+left join Suppliers s1 
+on o.ShipAddress= s1.Address
