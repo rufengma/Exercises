17017575d8a74163e7f371badaf3973118bacea7
Rufeng Ma <buguaixiaoya@gmail.com>
2021-06-23 09:54:36 -0400

    Add files via upload

---
+++ database/database2.sql
@@ -0,0 +1,13 @@
+create table lenders(LenderID int primary key, LenderName varchar (40), 
+AvailableMoney decimal (10,2))
+
+
+create table borrowers(BorrowerID int primary key, borrowerName varchar (40),
+RiskValue decimal (10,2))
+
+create table loans(LoanCode int primary key, BorrowerID int foreign key references borrowers(BorrowerID),
+LoanAmount decimal (10,2),DDL datetime, Rate decimal (5,2),Purpose varchar (100))
+
+create table lenderManage(LenderID int foreign key REFERENCES lenders(LenderID), 
+LoanCode int foreign key references loans(LoanCode))
+
